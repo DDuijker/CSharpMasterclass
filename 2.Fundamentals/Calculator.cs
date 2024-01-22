08359aac46d9331067dbddfa584e3f3ba9db4a78
DDuijker <djoekeduijker@live.nl>
2024-01-22 20:39:45 +0100

    Made Calculator

---
+++ 2.Fundamentals/Calculator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Fundamentals
+{
+    internal class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+ 
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Hello!\nInput the first number:");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input the second number:");
+            int secondNumber = int.Parse(Console.ReadLine());
+            //ask user for instructions
+            Console.WriteLine(
+                "What do you want to do with these numbers?\n" +
+                "[A]dd\n" +
+                "[S]ubtract\n" +
+                "[M]ultiply");
+
+            string userInput = Console.ReadLine().ToLower();
+            //handle the input 
+            if (userInput == "a")
+            {
+                Console.WriteLine($"{firstNumber} + {secondNumber} =  {Add(firstNumber, secondNumber)}");
+            }
+            else if (userInput == "s")
+            {
+                Console.WriteLine($"{firstNumber} - {secondNumber} =  {Subtract(firstNumber, secondNumber)}");
+            }
+            else if (userInput == "m")
+            {
+                Console.WriteLine($"{firstNumber} * {secondNumber} =  {Multiply(firstNumber, secondNumber)}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input");
+            }
+        }
+    }
+}
--- 2.Fundamentals/Program.cs
+++ 2.Fundamentals/Program.cs
@@ -1,10 +1,17 @@
 ﻿namespace _2.Fundamentals
 {
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello this is another test.");
+            Calculator calculator = new Calculator();
+            Console.WriteLine("Hello! I have made a calculator. Press 'S' to start: ");
+            if (Console.ReadLine().ToLower() == "s")
+            {
+                calculator.Start();
+            }
+            Console.ReadKey();
         }
     }
 }
